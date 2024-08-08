660e366528364a522e81d43b77752f1195cee82e
ShailjaRaii <shailja.rai@misteredtech.in>
2024-08-08 15:10:36 +0530

    Update insert_country.sql
    insert query for country table

--- database/postgresql/app/inserts/insert_country.sql
+++ database/postgresql/app/inserts/insert_country.sql
@@ -1,1 +1,193 @@
-select * from country;
+INSERT INTO country ("CountryID", "CountryName", "CountryISO2", "CountryISO3", "CreatedDate", "CreatedBy", "IsActive")
+select gen_random_uuid(),'Afghanistan','AF','AFG',now(),current_user,true union
+select gen_random_uuid(),'Albania','AL','ALB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Algeria','DZ','DZA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Andorra','AD','AND',current_timestamp,current_user,true union
+select gen_random_uuid(),'Angola','AO','AGO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Antigua and Barbuda','AG','ATG',current_timestamp,current_user,true union
+select gen_random_uuid(),'Argentina','AR','ARG',current_timestamp,current_user,true union
+select gen_random_uuid(),'Armenia','AM','ARM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Australia','AU','AUS',current_timestamp,current_user,true union
+select gen_random_uuid(),'Austria','AT','AUT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Azerbaijan','AZ','AZE',current_timestamp,current_user,true union
+select gen_random_uuid(),'Bahamas','BS','BHS',current_timestamp,current_user,true union
+select gen_random_uuid(),'Bahrain','BH','BHR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Bangladesh','BD','BGD',current_timestamp,current_user,true union
+select gen_random_uuid(),'Barbados','BB','BRB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Belarus','BY','BLR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Belgium','BE','BEL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Belize','BZ','BLZ',current_timestamp,current_user,true union
+select gen_random_uuid(),'Benin','BJ','BEN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Bhutan','BT','BTN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Bolivia','BO','BOL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Bosnia and Herzegovina','BA','BIH',current_timestamp,current_user,true union
+select gen_random_uuid(),'Botswana','BW','BWA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Brazil','BR','BRA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Brunei','BN','BRN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Bulgaria','BG','BGR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Burkina Faso','BF','BFA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Burundi','BI','BDI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Cabo Verde','CV','CPV',current_timestamp,current_user,true union
+select gen_random_uuid(),'Cambodia','KH','KHM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Cameroon','CM','CMR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Canada','CA','CAN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Central African Republic','CF','CAF',current_timestamp,current_user,true union
+select gen_random_uuid(),'Chad','TD','TCD',current_timestamp,current_user,true union
+select gen_random_uuid(),'Chile','CL','CHL',current_timestamp,current_user,true union
+select gen_random_uuid(),'China','CN','CHN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Colombia','CO','COL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Comoros','KM','COM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Congo, Democratic Republic of the','CD','COD',current_timestamp,current_user,true union
+select gen_random_uuid(),'Congo, Republic of the','CG','COG',current_timestamp,current_user,true union
+select gen_random_uuid(),'Costa Rica','CR','CRI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Croatia','HR','HRV',current_timestamp,current_user,true union
+select gen_random_uuid(),'Cuba','CU','CUB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Cyprus','CY','CYP',current_timestamp,current_user,true union
+select gen_random_uuid(),'Czech Republic','CZ','CZE',current_timestamp,current_user,true union
+select gen_random_uuid(),'Denmark','DK','DNK',current_timestamp,current_user,true union
+select gen_random_uuid(),'Djibouti','DJ','DJI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Dominica','DM','DMA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Dominican Republic','DO','DOM',current_timestamp,current_user,true union
+select gen_random_uuid(),'East Timor','TL','TLS',current_timestamp,current_user,true union
+select gen_random_uuid(),'Ecuador','EC','ECU',current_timestamp,current_user,true union
+select gen_random_uuid(),'Egypt','EG','EGY',current_timestamp,current_user,true union
+select gen_random_uuid(),'El Salvador','SV','SLV',current_timestamp,current_user,true union
+select gen_random_uuid(),'Equatorial Guinea','GQ','GNQ',current_timestamp,current_user,true union
+select gen_random_uuid(),'Eritrea','ER','ERI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Estonia','EE','EST',current_timestamp,current_user,true union
+select gen_random_uuid(),'Eswatini','SZ','SWZ',current_timestamp,current_user,true union
+select gen_random_uuid(),'Ethiopia','ET','ETH',current_timestamp,current_user,true union
+select gen_random_uuid(),'Fiji','FJ','FJI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Finland','FI','FIN',current_timestamp,current_user,true union
+select gen_random_uuid(),'France','FR','FRA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Gabon','GA','GAB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Gambia','GM','GMB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Georgia','GE','GEO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Germany','DE','DEU',current_timestamp,current_user,true union
+select gen_random_uuid(),'Ghana','GH','GHA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Greece','GR','GRC',current_timestamp,current_user,true union
+select gen_random_uuid(),'Grenada','GD','GRD',current_timestamp,current_user,true union
+select gen_random_uuid(),'Guatemala','GT','GTM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Guinea','GN','GIN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Guinea-Bissau','GW','GNB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Guyana','GY','GUY',current_timestamp,current_user,true union
+select gen_random_uuid(),'Haiti','HT','HTI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Honduras','HN','HND',current_timestamp,current_user,true union
+select gen_random_uuid(),'Hungary','HU','HUN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Iceland','IS','ISL',current_timestamp,current_user,true union
+select gen_random_uuid(),'India','IN','IND',current_timestamp,current_user,true union
+select gen_random_uuid(),'Indonesia','ID','IDN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Iran','IR','IRN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Iraq','IQ','IRQ',current_timestamp,current_user,true union
+select gen_random_uuid(),'Ireland','IE','IRL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Israel','IL','ISR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Italy','IT','ITA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Jamaica','JM','JAM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Japan','JP','JPN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Jordan','JO','JOR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Kazakhstan','KZ','KAZ',current_timestamp,current_user,true union
+select gen_random_uuid(),'Kenya','KE','KEN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Kiribati','KI','KIR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Kosovo','XK','XKO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Kuwait','KW','KWT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Kyrgyzstan','KG','KGZ',current_timestamp,current_user,true union
+select gen_random_uuid(),'Laos','LA','LAO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Latvia','LV','LVA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Lebanon','LB','LBN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Lesotho','LS','LSO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Liberia','LR','LBR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Libya','LY','LBY',current_timestamp,current_user,true union
+select gen_random_uuid(),'Liechtenstein','LI','LIE',current_timestamp,current_user,true union
+select gen_random_uuid(),'Lithuania','LT','LTU',current_timestamp,current_user,true union
+select gen_random_uuid(),'Luxembourg','LU','LUX',current_timestamp,current_user,true union
+select gen_random_uuid(),'Malawi','MW','MWI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Malaysia','MY','MYS',current_timestamp,current_user,true union
+select gen_random_uuid(),'Maldives','MV','MDV',current_timestamp,current_user,true union
+select gen_random_uuid(),'Mali','ML','MLI',current_timestamp,current_user,true union
+select gen_random_uuid(),'Malta','MT','MLT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Marshall Islands','MH','MHL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Mauritania','MR','MRT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Mauritius','MU','MUS',current_timestamp,current_user,true union
+select gen_random_uuid(),'Mexico','MX','MEX',current_timestamp,current_user,true union
+select gen_random_uuid(),'Micronesia','FM','FSM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Moldova','MD','MDA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Monaco','MC','MCO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Mongolia','MN','MNG',current_timestamp,current_user,true union
+select gen_random_uuid(),'Montenegro','ME','MNE',current_timestamp,current_user,true union
+select gen_random_uuid(),'Morocco','MA','MAR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Mozambique','MZ','MOZ',current_timestamp,current_user,true union
+select gen_random_uuid(),'Myanmar','MM','MMR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Namibia','NA','NAM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Nauru','NR','NRU',current_timestamp,current_user,true union
+select gen_random_uuid(),'Nepal','NP','NPL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Netherlands','NL','NLD',current_timestamp,current_user,true union
+select gen_random_uuid(),'New Zealand','NZ','NZL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Nicaragua','NI','NIC',current_timestamp,current_user,true union
+select gen_random_uuid(),'Niger','NE','NER',current_timestamp,current_user,true union
+select gen_random_uuid(),'Nigeria','NG','NGA',current_timestamp,current_user,true union
+select gen_random_uuid(),'North Macedonia','MK','MKD',current_timestamp,current_user,true union
+select gen_random_uuid(),'Norway','NO','NOR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Oman','OM','OMN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Pakistan','PK','PAK',current_timestamp,current_user,true union
+select gen_random_uuid(),'Palau','PW','PLW',current_timestamp,current_user,true union
+select gen_random_uuid(),'Panama','PA','PAN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Papua New Guinea','PG','PNG',current_timestamp,current_user,true union
+select gen_random_uuid(),'Paraguay','PY','PRY',current_timestamp,current_user,true union
+select gen_random_uuid(),'Peru','PE','PER',current_timestamp,current_user,true union
+select gen_random_uuid(),'Philippines','PH','PHL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Poland','PL','POL',current_timestamp,current_user,true union
+select gen_random_uuid(),'Portugal','PT','PRT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Qatar','QA','QAT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Romania','RO','ROU',current_timestamp,current_user,true union
+select gen_random_uuid(),'Russia','RU','RUS',current_timestamp,current_user,true union
+select gen_random_uuid(),'Rwanda','RW','RWA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Saint Kitts and Nevis','KN','KNA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Saint Lucia','LC','LCA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Saint Vincent and the Grenadines','VC','VCT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Samoa','WS','WSM',current_timestamp,current_user,true union
+select gen_random_uuid(),'San Marino','SM','SMR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Sao Tome and Principe','ST','STP',current_timestamp,current_user,true union
+select gen_random_uuid(),'Saudi Arabia','SA','SAU',current_timestamp,current_user,true union
+select gen_random_uuid(),'Senegal','SN','SEN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Serbia','RS','SRB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Seychelles','SC','SYC',current_timestamp,current_user,true union
+select gen_random_uuid(),'Sierra Leone','SL','SLE',current_timestamp,current_user,true union
+select gen_random_uuid(),'Singapore','SG','SGP',current_timestamp,current_user,true union
+select gen_random_uuid(),'Slovakia','SK','SVK',current_timestamp,current_user,true union
+select gen_random_uuid(),'Slovenia','SI','SVN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Solomon Islands','SB','SLB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Somalia','SO','SOM',current_timestamp,current_user,true union
+select gen_random_uuid(),'South Africa','ZA','ZAF',current_timestamp,current_user,true union
+select gen_random_uuid(),'South Sudan','SS','SSD',current_timestamp,current_user,true union
+select gen_random_uuid(),'Spain','ES','ESP',current_timestamp,current_user,true union
+select gen_random_uuid(),'Sri Lanka','LK','LKA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Sudan','SD','SDN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Suriname','SR','SUR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Sweden','SE','SWE',current_timestamp,current_user,true union
+select gen_random_uuid(),'Switzerland','CH','CHE',current_timestamp,current_user,true union
+select gen_random_uuid(),'Syria','SY','SYR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Taiwan','TW','TWN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Tajikistan','TJ','TJK',current_timestamp,current_user,true union
+select gen_random_uuid(),'Tanzania','TZ','TZA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Thailand','TH','THA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Togo','TG','TGO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Tonga','TO','TON',current_timestamp,current_user,true union
+select gen_random_uuid(),'Trinidad and Tobago','TT','TTO',current_timestamp,current_user,true union
+select gen_random_uuid(),'Tunisia','TN','TUN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Turkey','TR','TUR',current_timestamp,current_user,true union
+select gen_random_uuid(),'Turkmenistan','TM','TKM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Tuvalu','TV','TUV',current_timestamp,current_user,true union
+select gen_random_uuid(),'Uganda','UG','UGA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Ukraine','UA','UKR',current_timestamp,current_user,true union
+select gen_random_uuid(),'United Arab Emirates','AE','ARE',current_timestamp,current_user,true union
+select gen_random_uuid(),'United Kingdom','GB','GBR',current_timestamp,current_user,true union
+select gen_random_uuid(),'United States','US','USA',current_timestamp,current_user,true union
+select gen_random_uuid(),'Uruguay','UY','URY',current_timestamp,current_user,true union
+select gen_random_uuid(),'Uzbekistan','UZ','UZB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Vanuatu','VU','VUT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Vatican City','VA','VAT',current_timestamp,current_user,true union
+select gen_random_uuid(),'Venezuela','VE','VEN',current_timestamp,current_user,true union
+select gen_random_uuid(),'Vietnam','VN','VNM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Yemen','YE','YEM',current_timestamp,current_user,true union
+select gen_random_uuid(),'Zambia','ZM','ZMB',current_timestamp,current_user,true union
+select gen_random_uuid(),'Zimbabwe','ZW','ZWE',current_timestamp,current_user,true;
